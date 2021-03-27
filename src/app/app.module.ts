2b200339c732084d4143b1411be7db0f8cea8aa3
Javier <javier.gauna147@gmail.com>
2021-03-26 21:18:26 -0300

    proyecto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+
+/**
+ * Importanción para angular google maps
+*/
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyD9He2vKQMlBnlJr83dX4BIHwktBZg_yps'
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
